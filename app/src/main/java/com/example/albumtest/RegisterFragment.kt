b0b98339b280bb19d7c574e4073080b69edd2f43
yunjaem06 <yunjae0616@gmail.com>
2023-02-17 18:31:50 +0900

    baseFragment 작성

---
+++ app/src/main/java/com/example/albumtest/RegisterFragment.kt
@@ -0,0 +1,12 @@
+package com.example.albumtest
+
+import android.os.Bundle
+import android.view.View
+import com.example.albumtest.databinding.FragmentRegisterBinding
+
+class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterBinding::bind, R.layout.fragment_register) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
